9c6937b1d76e13e7405d1089f97b48cb4aad06dc
Jonathan Lehman <jonathan.lehman91@gmail.com>
2015-08-13 13:47:52 -0400

    Introduce state and click handler to Card component
    - Add flipped and matched states to Card
    - Toggle flipped state on click
    - Only render card value when card is flipped
    - Set classes on Card based on state (and add styles for these)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
+import classnames from 'classnames';
 
 export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      flipped: false,
+      matched: false
+    };
+  }
+
+  handleClick(e) {
+    var flipped = this.state.flipped;
+    this.setState({flipped: !flipped});
+  }
+
   render() {
+    var classes = classnames(
+      'Card',
+      {'Card--flipped': this.state.flipped},
+      {'Card--matched': this.state.matched}
+    );
+    var cardValue = this.state.flipped ? this.props.value : '';
     return (
-      <div className="Card">
-        {this.props.value}
+      <div className={classes} onClick={this.handleClick}>
+        {cardValue}
       </div>
     );
   }
